9cb320ddc5304d2424b15e8e84292268fafd5803
gabo <gabrielbarragan07@gmail.com>
2021-08-03 22:54:41 -0500

    Se realizan pruebas con querys y filters para traer diversos datos de la base de datos

--- Platzigram/posts/models.py
+++ Platzigram/posts/models.py
@@ -3,7 +3,8 @@
 #django
 from django.db import models 
 
-class user(models.Model):
+#Para crear modelos con django para diferentes motores de DB
+class user(models.Model):#Las tablas son representadas en una clase
     """User model."""
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
@@ -11,9 +12,15 @@ class user(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank=True)
     
     birthdate = models.DateField(blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """return email"""
+        return self.email
