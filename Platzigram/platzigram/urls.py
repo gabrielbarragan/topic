136356c1e79c05ec6b59577a996a3387eb75eff3
gabo <gabrielbarragan07@gmail.com>
2021-08-09 16:19:03 -0500

    creación del posts model y solución de la ruta de la media

--- Platzigram/platzigram/urls.py
+++ Platzigram/platzigram/urls.py
@@ -3,10 +3,13 @@
 #django
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from platzigram import views as local_view #se renombra para que no entre en conflicto con las de las apps
 
 from posts import views as post_views
+
 
 
 urlpatterns = [
@@ -17,4 +20,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_view.say_hi),
     
     path('posts/', post_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
