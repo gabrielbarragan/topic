8a3192dd703cc06735db21ebb9e4560ba9ef4e81
gabo <gabrielbarragan07@gmail.com>
2021-08-09 17:44:14 -0500

    Solución al reto de registrar el Post model

--- Platzigram/posts/admin.py
+++ Platzigram/posts/admin.py
@@ -1,3 +1,37 @@
+"""Posts admin classes"""
+#django
 from django.contrib import admin
 
+#models
+from posts.models import Post
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """posts admin."""
+    
+    list_display = ('user','Profile','title','created','modified')
+    list_display_links = ( 'title',)
+    list_editable = ( 'Profile',)
+
+    list_filter = (
+        'created',
+        'modified',
+        )
+
+    fieldsets = (
+        ('Post',{
+            'fields':(
+                'title','photo','Profile','user'
+        )
+        }),
+        ('Metadata',{
+            'fields':(
+                'created',
+                'modified',
+            )
+            }),
+        )
+
+
+    readonly_fields = ('created', 'modified',)
