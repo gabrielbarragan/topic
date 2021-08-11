45453700eec0f5a88a775e63ae0c7e3c30eaae0c
gabo <gabrielbarragan07@gmail.com>
2021-08-11 13:20:31 -0500

    Finalizado el signup e inicio de middlewares

--- Platzigram/users/views.py
+++ Platzigram/users/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+#exceptions
+from django.db.utils import IntegrityError
+
+#models 
+from django.contrib.auth.models import User
+from users.models import Profile
+
 
 def login_view(request):
     """login view"""
@@ -33,6 +40,37 @@ def logout_view(request):
     """logout a user"""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """ signup view"""
+
+    if request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      confirm_password = request.POST['confirm_password']
+      email = request.POST['email']
+      first_name = request.POST['first_name']
+      last_name = request.POST['last_name']
+
+      if password != confirm_password:
+          return render(request, 'users/signup.html',{'error': 'password confirmation does not match'})
+      try:
+          user = User.objects.create_user(username=username, password=password)
+
+      except IntegrityError:
+          return render(request, 'users/signup.html', {'error': f'User {username} already in use'})
+      
+      user.first_name = first_name
+      user.last_name = last_name
+      user.email = email
+      
+      user.save()
+
+      profile = Profile(user=user)
+      profile.save()
+      return redirect('login')
+
+    return render(request, 'users/signup.html')
 
 
 
