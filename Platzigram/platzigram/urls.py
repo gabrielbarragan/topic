ce0f27b1aacd07c80942f6e0fabd763e04550169
gabo <gabrielbarragan07@gmail.com>
2021-08-12 22:33:13 -0500

    se avanzó hasta el listado de posts

--- Platzigram/platzigram/urls.py
+++ Platzigram/platzigram/urls.py
@@ -6,8 +6,6 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from platzigram import views as local_view #se renombra para que no entre en conflicto con las de las apps
-
 from posts import views as post_views
 from users import views as users_views
 
@@ -16,10 +14,6 @@ from users import views as users_views
 
 urlpatterns = [
     path('admin/',admin.site.urls),
-
-    path('hello-world/', local_view.hello_world, name='hello_world'),
-    path('challenge/', local_view.challenge_sol_teacher, name='sort'),
-    path('hi/<str:name>/<int:age>/', local_view.say_hi, name='hi'),
     
     path('', post_views.list_posts, name='feed'),
     path('posts/new/', post_views.create_post, name='create_post'),
